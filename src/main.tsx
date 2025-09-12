e56970a365008849afaaddf4e5831e65fea2651b
milshanfernando <milshanfernando.mf@gmail.com>
2025-09-12 16:30:43 +0400

    refactor: Remove StrictMode and ReactQueryDevtools from main render

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { RouterProvider } from "react-router-dom";
 import router from "./routing/routes.tsx";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    {/* <ReactQueryDevtools /> */}
+  </QueryClientProvider>
 );
